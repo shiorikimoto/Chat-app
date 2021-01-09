085b48be0eaaf75d9bd3bdbc39e98c8b276e9b60
木本栞 <kimotoshiori@kimotonobuyukinoMacBook-Air.local>
2021-01-10 00:17:15 +0900

    Firebaseの導入

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDZ_o-tb0rZoaZvhpQzX2hI17PAT5lsi14",
+    authDomain: "chat-app-64384.firebaseapp.com",
+    projectId: "chat-app-64384",
+    storageBucket: "chat-app-64384.appspot.com",
+    messagingSenderId: "96234471466",
+    appId: "1:96234471466:web:31b239f4d588cdbdeb4353"
+}
+
+firebase.initializeApp(firebaseConfig)
+
+export default firebase
